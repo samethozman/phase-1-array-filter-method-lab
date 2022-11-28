5437360c437829661fe157b40bcb2db6c66ada84
samethozman <samet.hozman@outlook.com>
2022-11-28 14:15:15 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution here
+const findMatching = (array, query) => {
+    return array.filter(q => {
+        return q.toLowerCase() === query.toLowerCase();
+    })
+}
+
+const fuzzyMatch = (array, query) => {
+    const queryLength = query.length;
+    return array.filter(q => {
+        return q.slice(0,queryLength) === query;
+    })
+}
+
+const matchName = (array, query) => {
+    return array.filter(q => {
+        return q.name === query;
+    })
+}
